6e169579d33a4337f7ff4a977a582d150eb23264
sagnolkamdem <77138358+sagnolkamdem@users.noreply.github.com>
2021-03-12 09:16:48 +0100

    Add files via upload

--- CV/settings.py
+++ CV/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'CV.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cv',
-#         'USER': 'sagnol',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'cv'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cv',
+        'USER': 'sagnol',
+        'PASSWORD': 'root',
+        'HOST': '',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'cv'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
